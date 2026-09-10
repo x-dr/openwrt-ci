a536748ba287a963a56e6d433751ff9bf6dd0a02
x-dr <79109673+x-dr@users.noreply.github.com>
2026-09-11 00:35:36 +0800

    Remove OpenClash and Nikki from builds
    * Remove Nikki from IPQ60XX WiFi build
    
    * Remove OpenClash from X86-64 build
    
    * Remove OpenClash and Nikki from X86-64 K6.18 build

--- libwrt.sh
+++ libwrt.sh
@@ -4,14 +4,6 @@ chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app
 
 # Extra packages: only enable for the IPQ60XX 6.12 WiFi profile
 if [ "$CONFIG_FILE" = "configs/ipq60xx-6.12-wifi.config" ]; then
-  # Nikki
-  grep -q '^src-git nikki ' feeds.conf.default || echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >> feeds.conf.default
-  ./scripts/feeds update nikki
-  ./scripts/feeds install -a -p nikki
-  grep -q '^CONFIG_PACKAGE_nikki=y$' .config || echo 'CONFIG_PACKAGE_nikki=y' >> .config
-  grep -q '^CONFIG_PACKAGE_luci-app-nikki=y$' .config || echo 'CONFIG_PACKAGE_luci-app-nikki=y' >> .config
-  grep -q '^CONFIG_PACKAGE_luci-i18n-nikki-zh-cn=y$' .config || echo 'CONFIG_PACKAGE_luci-i18n-nikki-zh-cn=y' >> .config
-
   # KMS (vlmcsd) + LuCI UI + Simplified Chinese translation
   grep -q '^CONFIG_PACKAGE_vlmcsd=y$' .config || echo 'CONFIG_PACKAGE_vlmcsd=y' >> .config
   grep -q '^CONFIG_PACKAGE_luci-app-vlmcsd=y$' .config || echo 'CONFIG_PACKAGE_luci-app-vlmcsd=y' >> .config
